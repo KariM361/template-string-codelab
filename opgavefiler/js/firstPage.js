cc182f444c194cd69b5e07aa53bff1491463a33d
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-09-02 10:26:07 +0200

    opgave 1 og 2

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -2,12 +2,59 @@
 
 
 // din kode her
+const navn = "Keld";
+const aar = 3;
+const besked = `<h2>Hej ${navn}</h2> du har været ansat i vores virksomhed i ${aar} år`;
 
+const element = document.getElementById("message");
+element.innerHTML = besked;
 
-/* Opgave 1*/
-
+/* Opgave 2*/
 
 // din kode her
+const myData =[{
+    "navn": "Sko",
+    "beskrivelse": "et par sko i læder",
+    "pris":200
+},
+    {
+    "navn":"Sandaler",
+     "beskrivelse": "et par sandaler i ruskind",
+    "pris":400
+    },
+     {
+    "navn":"Støvler",
+     "beskrivelse": "et par støvler i sort",
+    "pris":500
+    }]
+    // const element = document.getElementById("ProduktListe");
+    //                 element.innerHTML = myData;
+
+ const produktListe = document.getElementById("produktListe")
+ let HTML = "";
+myData.forEach ((produkt) => {
+HTML += `<article class = "produkt">
+<h2>${produkt.navn}</h2>
+<p>${produkt.beskrivelse}</p>
+<p>${produkt.pris}</p>
+</article>`
+})
+ produktListe.innerHTML= HTML;
+                
+// const myData =[{
+//     "navn": "t-shirt"   
+//      "beskrivelse": "En t-shirt lavet af bomuld",
+//      "pris": 99 },
+//      { "navn": "Jeans",
+//     "beskrivelse": "Blå denim jeans med straight fit",
+//      "pris": 299
+//      },
+//     { "navn": "Hættetrøje",
+//      "beskrivelse": "En grå hættetrøje med lynlås",
+//       "pris": 199 }
+              
+// }]
+
 
 
 
